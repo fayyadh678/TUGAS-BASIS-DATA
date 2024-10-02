b21555157b54d444e90c4c0a995ef725a4e4734d
fayyadh678 <118834680+fayyadh678@users.noreply.github.com>
2024-10-02 13:42:10 +0700

    Add files via upload

---
+++ QUIZ5.sql
@@ -0,0 +1,6 @@
+SELECT * FROM quiz5.invoices;
+SELECT Total + 0.25 FROM invoices;
+SELECT Total - 0.15 FROM invoices;
+SELECT Total * 2 FROM invoices;
+SELECT Total / 2 FROM invoices;
+SELECT Total % 2 FROM invoices;
